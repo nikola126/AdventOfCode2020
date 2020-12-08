1561b59d956398c499ab1cd1af2c5f9d931d7bb5
Nikola126 <nikj1705@gmail.com>
2020-12-08 14:52:01 +0200

    Day 6 Complete

--- main.py
+++ main.py
@@ -1,4 +1,59 @@
 # Day 6
 
 if __name__ == '__main__':
-    pass
+
+    # unique_questions = {}
+    # unique_answers = 0
+    # for line in open('puzzle_input.txt', 'r').readlines():
+    #     line = line.strip('\n')
+    #     if line == '':
+    #         # new group, count the unique answers and reset dictionary
+    #         unique_answers += len(unique_questions.keys())
+    #         unique_questions = {}
+    #     else:
+    #         for char in line:
+    #             # add unique answers to dictionary
+    #             if char not in unique_questions.keys():
+    #                 unique_questions[char] = 1
+    #             else:
+    #                 pass
+    # # check last line
+    # unique_answers += len(unique_questions.keys())
+    # unique_questions = {}
+    #
+    # print(unique_answers)
+
+    # PART TWO
+    print("PART TWO")
+    answers = {}
+    everyone = 0
+
+    people = 0
+    for line in open('puzzle_input.txt', 'r').readlines():
+        line = line.strip('\n')
+        if line == '':
+            # new group
+            print(f"Questions to which all {people} people answered:")
+            for key in answers.keys():
+                if answers[key] == people:
+                    print(key)
+                    everyone += 1
+            people = 0
+            answers = {}
+        else:
+            people += 1
+            for char in line:
+                # add unique answers to dictionary
+                if char not in answers.keys():
+                    answers[char] = 1
+                else:
+                    answers[char] += 1
+
+    # check last line
+    print(f"Questions to which all {people} people answered:")
+    for key in answers.keys():
+        if answers[key] == people:
+            print(key)
+            everyone += 1
+
+    print(everyone)
