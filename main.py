66dcebfc277c5e0284f4171a61e7f7cce403a86c
Nikola126 <nikj1705@gmail.com>
2020-12-08 14:52:27 +0200

    Day 6 complete

--- main.py
+++ main.py
@@ -2,26 +2,26 @@
 
 if __name__ == '__main__':
 
-    # unique_questions = {}
-    # unique_answers = 0
-    # for line in open('puzzle_input.txt', 'r').readlines():
-    #     line = line.strip('\n')
-    #     if line == '':
-    #         # new group, count the unique answers and reset dictionary
-    #         unique_answers += len(unique_questions.keys())
-    #         unique_questions = {}
-    #     else:
-    #         for char in line:
-    #             # add unique answers to dictionary
-    #             if char not in unique_questions.keys():
-    #                 unique_questions[char] = 1
-    #             else:
-    #                 pass
-    # # check last line
-    # unique_answers += len(unique_questions.keys())
-    # unique_questions = {}
-    #
-    # print(unique_answers)
+    unique_questions = {}
+    unique_answers = 0
+    for line in open('puzzle_input.txt', 'r').readlines():
+        line = line.strip('\n')
+        if line == '':
+            # new group, count the unique answers and reset dictionary
+            unique_answers += len(unique_questions.keys())
+            unique_questions = {}
+        else:
+            for char in line:
+                # add unique answers to dictionary
+                if char not in unique_questions.keys():
+                    unique_questions[char] = 1
+                else:
+                    pass
+    # check last line
+    unique_answers += len(unique_questions.keys())
+    unique_questions = {}
+
+    print(unique_answers)
 
     # PART TWO
     print("PART TWO")
