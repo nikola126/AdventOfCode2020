92a4d09ccf47ceaa9872cd11f67c3f0042579894
Nikola <nikj1705@gmail.com>
2020-12-08 09:30:33 +0200

    day 2 added

--- main.py
+++ main.py
@@ -1,48 +1,62 @@
-# Day 1
+# Day 2
 
 # Get Input
 if __name__ == '__main__':
 
-    report = []
+    pwd_list = []
     for line in open("puzzle_input.txt", "r"):
         line = line.strip('\n')
-        line = int(line)
-        report.append(line)
-
-    entries = len(report)
-    print(f"Report with {entries} entries:\n", report)
+        pwd_list.append(line)
 
     # PART ONE
-    # Loop Through Values
-    offset = 1
-    for entry_one in report:
-        for entry_two in report[offset:]:
-            sum = entry_one + entry_two
-            if sum == 2020:
-                result = entry_one * entry_two
-                print(result)
-        offset = offset + 1
+
+    entries = len(pwd_list)
+    print(f"Report with {entries} entries")
+
+    valid_passwords = 0
+    # Loop through values
+    for entry in pwd_list:
+        # Get Password Policy
+        entry = entry.split(' ')
+        # Lower/Upper Count Limits
+        indices = entry[0].split('-')
+        lower = int(indices[0])
+        upper = int(indices[1])
+        # Character to look for
+        char = entry[1].strip(':')
+        # Password
+        password = entry[2]
+
+        # Info
+        # print(f"{char} must be in {password} between {lower} and {upper} times ({password.count(char)}).")
+        if lower <= password.count(char) <= upper:
+            valid_passwords = valid_passwords + 1
+
+    print(valid_passwords)
 
     # PART TWO
-    # Loop Through Values
-    offset_one_initial = 1
-    offset_two_initial = 2
-    offset_one = 1
-    offset_two = 2
-    for entry_one in report:
-        # print(f"Offsets: {offset_one}, {offset_two}")
-        # print(f"Looking at {entry_one}")
-        for entry_two in report[offset_one:]:
-            # print(f"\tLooking at {entry_two}")
-            for entry_three in report[offset_two:]:
-                # print(f"\t\tLooking at {entry_three}")
-                # print(f"\t\tLooking at {entry_one}, {entry_two} and {entry_three}")
-                if (entry_one + entry_two + entry_three) == 2020:
-                    result = (entry_one * entry_two * entry_three)
-                    print(result)
-            offset_two = offset_two + 1
-        # Reset Offsets
-        offset_one_initial = offset_one_initial + 1
-        offset_two_initial = offset_two_initial + 1
-        offset_one = offset_one_initial
-        offset_two = offset_two_initial
+    valid_passwords = 0
+    # Loop through values
+    for entry in pwd_list:
+        # Get Password Policy
+        entry = entry.split(' ')
+        # Get Position Indices
+        indices = entry[0].split('-')
+        index_one = int(indices[0])
+        index_two = int(indices[1])
+        # Character to look for
+        char = entry[1].strip(':')
+        # Password
+        password = entry[2]
+        # Get Letters at those Indices
+        letter_one = password[index_one-1]
+        letter_two = password[index_two-1]
+
+        if (letter_one == char) ^ (letter_two == char):
+            valid_passwords = valid_passwords + 1
+
+    print(valid_passwords)
+
+
+
+
