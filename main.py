2f7555e0db1522cc5f7d9b98e662b8e9eee661a0
Nikola126 <nikj1705@gmail.com>
2020-12-08 16:54:14 +0200

    Day 7 Part 1 Complete

--- main.py
+++ main.py
@@ -1,59 +1,65 @@
-# Day 6
+# Day 7
+
+bags_checked = set()
+
+
+def bag_check(bags_list, target, current_bag):
+    if target in current_bag['contains']:
+        bags_checked.add(current_bag['name'])
+        return True
+    else:
+        # RECURSIVE CALL
+        for bag in current_bag['contains']:
+            if bag in bags_checked:
+                bags_checked.add(current_bag['name'])
+                return True
+        if len(current_bag['contains']) == 0:
+            pass
+        else:
+            for item in current_bag['contains']:
+                # find the dictionary entry
+                for entry in bags_list:
+                    if entry['name'] == item:
+                        check_here = entry
+                if bag_check(bags_list, target, check_here):
+                    bags_checked.add(current_bag['name'])
+                    return True
+
 
 if __name__ == '__main__':
 
-    unique_questions = {}
-    unique_answers = 0
+    # create list of dictionaries
+    bags_list = []
+    # light_red_bag = { inside: [bright_white, muted_yellow] max:[1 2] }
     for line in open('puzzle_input.txt', 'r').readlines():
         line = line.strip('\n')
-        if line == '':
-            # new group, count the unique answers and reset dictionary
-            unique_answers += len(unique_questions.keys())
-            unique_questions = {}
+        line = line.split(" ")
+        name = line[0] + line[1]
+        bag = {'name': name}
+        # check for other bags inside
+        if line[4] == 'no':
+            bag['contains'] = []
+            bag['amounts'] = []
         else:
-            for char in line:
-                # add unique answers to dictionary
-                if char not in unique_questions.keys():
-                    unique_questions[char] = 1
-                else:
-                    pass
-    # check last line
-    unique_answers += len(unique_questions.keys())
-    unique_questions = {}
+            # isolate rest of the description
+            description = line[4:]
+            # how many inside ?
+            bags_inside = len(description) / 4
+            bag['contains'] = []
+            bag['amounts'] = []
+            for i in range(0, int(bags_inside)):
+                bag_inside = description[1 + 4 * i] + description[2 + 4 * i]
+                amount_inside = description[0 + 4 * i]
+                bag['contains'].append(bag_inside)
+                bag['amounts'].append(amount_inside)
+        bags_list.append(bag)
 
-    print(unique_answers)
+    target = 'shinygold'
+    print(f"TARGET:", target)
 
-    # PART TWO
-    print("PART TWO")
-    answers = {}
-    everyone = 0
+    # check top level
+    for bag in bags_list:
+        bag_check(bags_list, target, bag)
 
-    people = 0
-    for line in open('puzzle_input.txt', 'r').readlines():
-        line = line.strip('\n')
-        if line == '':
-            # new group
-            print(f"Questions to which all {people} people answered:")
-            for key in answers.keys():
-                if answers[key] == people:
-                    print(key)
-                    everyone += 1
-            people = 0
-            answers = {}
-        else:
-            people += 1
-            for char in line:
-                # add unique answers to dictionary
-                if char not in answers.keys():
-                    answers[char] = 1
-                else:
-                    answers[char] += 1
-
-    # check last line
-    print(f"Questions to which all {people} people answered:")
-    for key in answers.keys():
-        if answers[key] == people:
-            print(key)
-            everyone += 1
-
-    print(everyone)
+    # print("SET:", set(bags_checked))
+    print("LENGTH:", len(set(bags_checked)))
