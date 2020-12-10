eee8a9a543304ed7502bda53a180d265d3f24dee
Nikola <nikj1705@gmail.com>
2020-12-10 11:14:09 +0200

    day 10 wip

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ if __name__ == '__main__':
     # Get input
     data = []
     for line in open('puzzle_input.txt', 'r').readlines():
-        data.append(line)
+        line = line.strip('\n')
+        data.append(int(line))
+
+    start_voltage = 0
+    i = 0
+    reps = 0
+    current_voltage = 0
+    while True:
+        target = current_voltage + 1
+        print(f"Now:{current_voltage} Look for: {target}")
+        if target not in data:
+            pass
+        elif target+3 not in data:
+            break
+        else:
+            current_voltage = target
+            target += 1
 
 
